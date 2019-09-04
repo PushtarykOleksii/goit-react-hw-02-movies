35738bb0e803dbe1265927a431c2973c1c261e3f
Pushtaryk Oleksii <pushtarik@gmail.come>
2019-09-04 20:37:26 +0300

    Updates

---
+++ precache-manifest.d6ce831e34411144b0875bf5a18702ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e31e13efe3879f49325c4364dc74b7",
+    "url": "/goit-react-hw-02-movies/index.html"
+  },
+  {
+    "revision": "e2d235c77f488eb92ca7",
+    "url": "/goit-react-hw-02-movies/static/css/main.8d796025.chunk.css"
+  },
+  {
+    "revision": "2e692b0cb20088025134",
+    "url": "/goit-react-hw-02-movies/static/js/2.dd3f2ad8.chunk.js"
+  },
+  {
+    "revision": "e2d235c77f488eb92ca7",
+    "url": "/goit-react-hw-02-movies/static/js/main.77fadee1.chunk.js"
+  },
+  {
+    "revision": "0069d2c8e0e7d1c56d0e",
+    "url": "/goit-react-hw-02-movies/static/js/runtime~main.58f451ba.js"
+  }
+]);
